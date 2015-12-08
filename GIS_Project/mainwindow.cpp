7524b801067470e8a423a1b78244d3e03873d18d
houyanqiang <1183878993@qq.com>
2015-12-09 00:23:19 +0800

    The third test

--- GIS_Project/mainwindow.cpp
+++ GIS_Project/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QDebug>
 #include <QWidget>
+#include <iostream>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
